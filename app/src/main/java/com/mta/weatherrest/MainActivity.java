31cbb9d861f9ca8f3eb26b70d9b4d3b474f6f53a
amir <amir@mindtheapps.com>
2017-06-02 01:10:11 +0300

    added a query parameter

--- app/src/main/java/com/mta/weatherrest/MainActivity.java
+++ app/src/main/java/com/mta/weatherrest/MainActivity.java
@@ -4,19 +4,29 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatEditText;
+import android.view.KeyEvent;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 
 import com.mta.model.WeatherResponse;
 import com.mta.util.RetrofitClient;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
+    @BindView(R.id.navigation)
+    BottomNavigationView navigation;// = (BottomNavigationView) findViewById(R.id.navigation);
+    @BindView(R.id.message)
+    TextView mTextMessage;
+    @BindView(R.id.location)
+    AppCompatEditText mWhere;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -25,11 +35,13 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                    if (mWhere.getText().length() == 0) {
+                        mWhere.setText("Ramat Gan,il");
+                    }
+                    getWeather(mWhere.getText().toString());
                     return true;
                 case R.id.navigation_dashboard:
                     mTextMessage.setText(R.string.title_current_weather);
-                    getWeather();
                     return true;
                 case R.id.navigation_notifications:
                     mTextMessage.setText(R.string.title_notifications);
@@ -48,17 +60,17 @@ public class MainActivity extends AppCompatActivity {
      * paste here:
      * http://www.jsonschema2pojo.org/
      */
-    private void getWeather() {
+    private void getWeather(String city) {
         String baseUrl = getResources().getString(R.string.weather_base_url);
         String key = getResources().getString(R.string.weather_api_key);
 
         WeatherService weatherService = RetrofitClient.getClient(baseUrl).create(WeatherService.class);
 
-        Call<WeatherResponse> weather = weatherService.getWeather();
+        Call<WeatherResponse> weather = weatherService.getWeather(city);
         weather.enqueue(new Callback<WeatherResponse>() {
             @Override
             public void onResponse(Call<WeatherResponse> call, Response<WeatherResponse> response) {
-                mTextMessage.setText("It's "+response.body().getMain().getTemp()+" degrees currently in "+response.body().getName());
+                mTextMessage.setText("It's " + response.body().getMain().getTemp() + " degrees currently in " + response.body().getName());
             }
 
             @Override
@@ -74,9 +86,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        mWhere.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                // If the event is a key-down event on the "enter" button
+                if ((event.getAction() == KeyEvent.ACTION_DOWN) &&
+                        (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                    getWeather(mWhere.getText().toString());
+                    return true;
+                }
+                return false;
+            }
+        });
+//        mTextMessage = (TextView) findViewById(R.id.message);
+//        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
--- app/src/main/java/com/mta/weatherrest/WeatherService.java
+++ app/src/main/java/com/mta/weatherrest/WeatherService.java
@@ -4,6 +4,7 @@ import com.mta.model.WeatherResponse;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Query;
 
 /**
  * Created by amir on 6/1/17.
@@ -13,5 +14,16 @@ public interface WeatherService {
 
     @GET("weather?q=Givatayim,il&appid=15cf9b712a8454b5fcd0670649018163&units=metric")
     Call<WeatherResponse> getWeather();
+
+    /**
+     * http://square.github.io/retrofit/2.x/retrofit/retrofit2/http/Query.html
+     *
+     * The @Query("q") annotation appends "&q=..the_city.." to the end of the GET command
+     *
+     * @param city
+     * @return
+     */
+    @GET("weather?appid=15cf9b712a8454b5fcd0670649018163&units=metric")
+    Call<WeatherResponse> getWeather(@Query("q") String city);
 
 }
